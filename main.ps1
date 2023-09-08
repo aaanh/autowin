3fb162c8ecca4c73b70d07ce21af5e0fff292bf0
Anh Nguyen <anhnguyen@aaanh.com>
2023-09-08 14:57:32 -0400

    Added workflows

--- main.ps1
+++ main.ps1
@@ -14,13 +14,6 @@ Write-Host "Disabling News and Interests..."
 
 Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" -Name "ShellFeedsTaskbarViewMode" -Value 2
 
-# 3. Install Google Chrome
-Write-Host "Now installing Chrome"
-# Download Chrome installer to tempdir
-Invoke-WebRequest -Uri "https://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $env:TEMP\chrome_installer.exe
-# Install Chrome
-Start-Process -FilePath $env:TEMP\chrome_installer.exe -Args "/silent /install" -Verb RunAs -Wait
-
 # 4. Enable optional features
 $win_features = "HypervisorPlatform", "VirtualMachinePlatform", "Microsoft-Windows-Subsystem-Linux", "Microsoft-Hyper-V-All", "Microsoft-Hyper-V", "Microsoft-Hyper-V-Tools-All", "Microsoft-Hyper-V-Management-Powershell", "Microsoft-Hyper-V-Hypervisor", "Microsoft-Hyper-V-Services", "Microsoft-Hyper-V-Management-Clients"
 
@@ -29,11 +22,12 @@ ForEach ($0 in $win_features) {
   dism /online /enable-feature /featurename:$0 /All /NoRestart
 }
 
-# 5. Install dev tools
-# Visual Studio Code
-& "$PSScriptRoot\Install-DevTools\vscode.ps1"
+# 5. Install apps and dev tools
 # Git
 & "$PSScriptRoot\Install-DevTools\git.ps1"
+# Visual Studio Code
+& "$PSScriptRoot\Install-DevTools\vscode.ps1"
 # Node Version Manager
 & "$PSScriptRoot\Install-DevTools\nvm.ps1"
-
+# Chrome
+& "$PSScriptRoot\Install-DevTools\chrome.ps1"
